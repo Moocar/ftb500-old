9f05a6acd5dacb84a26cee48c3074774a3ab0b08
Anthony Marcar <amarcar@walmartlabs.com>
2014-04-08 22:20:43 -0700

    Formatting

--- src/me/moocar/ftb500/kitty.clj
+++ src/me/moocar/ftb500/kitty.clj
@@ -19,22 +19,28 @@
         game (first (:game/_seats seat))
         game-id (:db/id game)
         bids (bids/get-bids game)]
-    (if-not (bids/finished? bids
-                            (count (:game/seats game)))
-      (throw (ex-info "Bidding round not finished!"
-                      {:seat seat
-                       :cards cards}))
-      (if (exchanged? game)
-        (throw (ex-info "Kitty already exchanged!"
-                        {:seat seat
-                         :cards cards}))
-        (if-not (= seat (:game.bid/seat (bids/winning-bid bids)))
-          (throw (ex-info "You didn't win the bid"
-                          {:seat seat
-                           :cards cards}))
-          (let [current-kitty (:game.kitty/cards game)
-                tx (concat (map #(vector :db/retract game-id :game.kitty/cards (:db/id %))
-                                current-kitty)
-                           (map #(vector :db/add game-id :game.kitty/cards (:db/id %))
-                                cards))]
-            @(d/transact conn tx)))))))
+    (cond
+
+     (not (bids/finished? bids (count (:game/seats game))))
+     (throw (ex-info "Bidding round not finished!"
+                     {:seat seat
+                      :cards cards}))
+
+     (exchanged? game)
+     (throw (ex-info "Kitty already exchanged!"
+                     {:seat seat
+                      :cards cards}))
+
+     (not= seat (:game.bid/seat (bids/winning-bid bids)))
+     (throw (ex-info "You didn't win the bid"
+                     {:seat seat
+                      :cards cards})))
+
+    (let [current-kitty (:game.kitty/cards game)
+          tx (concat (map #(vector :db/retract game-id
+                                   :game.kitty/cards (:db/id %))
+                          current-kitty)
+                     (map #(vector :db/add game-id
+                                   :game.kitty/cards (:db/id %))
+                          cards))]
+      @(d/transact conn tx))))
