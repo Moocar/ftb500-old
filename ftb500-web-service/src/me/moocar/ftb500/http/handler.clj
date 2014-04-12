dd62360848790804d570c063773bc4685f738a4c
Anthony Marcar <amarcar@walmartlabs.com>
2014-04-12 15:30:50 -0700

    Added handlers and edn

--- ftb500-web-service/src/me/moocar/ftb500/http/handler.clj
+++ ftb500-web-service/src/me/moocar/ftb500/http/handler.clj
@@ -1,11 +1,42 @@
 (ns me.moocar.ftb500.http.handler
   (:require [com.stuartsierra.component :as component]))
 
+(defn default-handler
+  [request]
+  {:status 404
+   :body {:msg "No Service Found"}})
+
+(defn make-handler
+  [component]
+  (fn [request]
+    (default-handler request)))
+
+(defn wrap-catch-error
+  [this handler]
+  (fn [request]
+    (try
+      (handler request)
+      (catch Throwable t
+        (println "got error" t)
+        (.printStackTrace t)
+        {:status 500
+         :body {:msg "Internal Server Error"}}))))
+
+(defn wrap-edn-response
+  [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (-> response
+          (update-in [:body] pr-str)
+          (update-in [:headers] assoc "Content-Type" "application/edn")))))
+
 (defrecord HandlerComponent []
   component/Lifecycle
   (start [this]
     (assoc this
-      :handler (fn [request] {:status 200 :body "Great success"})))
+      :handler (-> (make-handler this)
+                   (->> (wrap-catch-error this))
+                   (wrap-edn-response))))
   (stop [this]
     this))
 
