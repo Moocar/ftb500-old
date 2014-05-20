8fb396b88199ae52016df1033ffbe3e4b61077fb
Anthony Marcar <amarcar@walmartlabs.com>
2014-05-19 23:39:46 -0700

    Removed cruft from engine project.clj

--- ftb500-engine/project.clj
+++ ftb500-engine/project.clj
@@ -1,8 +1,4 @@
 (defproject me.moocar/ftb500 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [com.datomic/datomic-free "0.9.4699"]
