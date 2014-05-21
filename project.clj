58066c3e5e8da470e7807c03aa95028f25732f67
Anthony Marcar <amarcar@walmartlabs.com>
2014-05-20 18:50:55 -0700

    Added head project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject me.moocar/ftb500 "0.1.0-SNAPSHOT"
+  :plugins [[lein-sub "0.2.4"]]
+  :sub ["ftb500-protocols"
+        "ftb500-client"
+        "ftb500-engine"
+        "ftb500-engine-client"])
