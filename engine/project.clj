cc6df9a1559b734dcc3d879985e6451fcbe6685f
Anthony Marcar <amarcar@walmartlabs.com>
2014-10-25 11:20:16 -0700

    Upgraded dependencies

--- engine/project.clj
+++ engine/project.clj
@@ -3,9 +3,10 @@
   including the database layer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
-                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
-                 [com.datomic/datomic-free "0.9.4880.2"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [com.datomic/datomic-free "0.9.4956"]
                  [com.stuartsierra/component "0.2.2"]
                  [me.moocar/log "0.1.0-SNAPSHOT"]
-                 [me.moocar.ftb500/pure-game "0.1.0-SNAPSHOT"]])
+                 [me.moocar.ftb500/pure-game "0.1.0-SNAPSHOT"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.5.9"]]}})
