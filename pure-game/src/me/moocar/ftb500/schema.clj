cad3ca869d14a0e950506c88d6384f8ab36ac0a6
Anthony Marcar <amarcar@walmartlabs.com>
2014-10-25 13:26:08 -0700

    Put bid names in set

--- pure-game/src/me/moocar/ftb500/schema.clj
+++ pure-game/src/me/moocar/ftb500/schema.clj
@@ -16,34 +16,37 @@
 (def Deck
   {:deck/num-players Number})
 
+(def bid-names
+  #{:bid.name/six-spades
+    :bid.name/six-clubs
+    :bid.name/six-diamonds
+    :bid.name/six-hearts
+    :bid.name/six-no-trumps
+    :bid.name/seven-spades
+    :bid.name/seven-clubs
+    :bid.name/seven-diamonds
+    :bid.name/seven-hearts
+    :bid.name/seven-no-trumps
+    :bid.name/eight-spades
+    :bid.name/eight-clubs
+    :bid.name/eight-diamonds
+    :bid.name/eight-hearts
+    :bid.name/eight-no-trumps
+    :bid.name/nine-spades
+    :bid.name/nine-clubs
+    :bid.name/nine-diamonds
+    :bid.name/nine-hearts
+    :bid.name/nine-no-trumps
+    :bid.name/ten-spades
+    :bid.name/ten-clubs
+    :bid.name/ten-diamonds
+    :bid.name/ten-hearts
+    :bid.name/ten-no-trumps
+    :bid.name/misere
+    :bid.name/open-misere})
+
 (def BidName
-  (s/enum :bid.name/six-spades
-          :bid.name/six-clubs
-          :bid.name/six-diamonds
-          :bid.name/six-hearts
-          :bid.name/six-no-trumps
-          :bid.name/seven-spades
-          :bid.name/seven-clubs
-          :bid.name/seven-diamonds
-          :bid.name/seven-hearts
-          :bid.name/seven-no-trumps
-          :bid.name/eight-spades
-          :bid.name/eight-clubs
-          :bid.name/eight-diamonds
-          :bid.name/eight-hearts
-          :bid.name/eight-no-trumps
-          :bid.name/nine-spades
-          :bid.name/nine-clubs
-          :bid.name/nine-diamonds
-          :bid.name/nine-hearts
-          :bid.name/nine-no-trumps
-          :bid.name/ten-spades
-          :bid.name/ten-clubs
-          :bid.name/ten-diamonds
-          :bid.name/ten-hearts
-          :bid.name/ten-no-trumps
-          :bid.name/misere
-          :bid.name/open-misere))
+  (apply s/enum bid-names))
 
 (def Bid
   {:bid/name BidName
