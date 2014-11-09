913f5b31ed1815d9ab1205a96342acfba8e37ef9
Anthony Marcar <amarcar@walmartlabs.com>
2014-11-08 17:33:18 -0800

    Use new ext card schema in find card

--- engine/src/me/moocar/ftb500/engine/card.clj
+++ engine/src/me/moocar/ftb500/engine/card.clj
@@ -1,5 +1,6 @@
 (ns me.moocar.ftb500.engine.card
-  (:require [datomic.api :as d])
+  (:require [datomic.api :as d]
+            [me.moocar.ftb500.schema :as schema :refer [ext-card?]])
   (:refer-clojure :exclude [find]))
 
 (defn suit-ext-form
@@ -10,8 +11,8 @@
   [card]
   (-> card
       (select-keys [:card/suit :card/rank])
-      (update-in [:card/suit] :card.suit/name)
-      (update-in [:card/rank] :card.rank/name)))
+      (update-in [:card/suit] select-keys [:card.suit/name])
+      (update-in [:card/rank] select-keys [:card.rank/name])))
 
 (defn partition-hands
   "Partitions a deck an into 4 hands of 10 cards each. Returns a map
@@ -33,27 +34,21 @@
       (->> (d/entity db))))
 
 (defn find
-  "Given a card in the format {:suit :hearts :rank :five}, returns
-  that card's entity. Note a joker does not have a suit and is
-  represented as {:rank :joker}"
-  [db {:keys [suit rank]}]
-  (if suit
-    (let [full-suit (keyword "card.suit.name" (name suit))
-          full-rank (keyword "card.rank.name" (name rank))]
-      (-> '[:find ?card
-            :in $ ?suit-name ?rank-name
-            :where [?suit :card.suit/name ?suit-name]
-            [?rank :card.rank/name ?rank-name]
-            [?card :card/suit ?suit]
-            [?card :card/rank ?rank]]
-          (d/q db full-suit full-rank)
-          (ffirst)
-          (->> (d/entity db))))
-    (let [full-rank (keyword "card.rank.name" (name rank))]
-      (-> '[:find ?card
-            :in $ ?rank-name
-            :where [?rank :card.rank/name ?rank-name]
-            [?card :card/rank ?rank]]
-          (d/q db full-rank)
-          (ffirst)
-          (->> (d/entity db))))))
+  "Given an ext-card, returns that card's entity"
+  [db {:keys [card/suit card/rank] :as card}]
+  {:pre [(ext-card? card)]}
+  (-> (if suit
+        (-> '[:find ?card
+              :in $ ?suit-name ?rank-name
+              :where [?suit :card.suit/name ?suit-name]
+              [?rank :card.rank/name ?rank-name]
+              [?card :card/suit ?suit]
+              [?card :card/rank ?rank]]
+            (d/q db (:card.suit/name suit) (:card.rank/name rank)))
+        (-> '[:find ?card
+              :in $ ?rank-name
+              :where [?rank :card.rank/name ?rank-name]
+              [?card :card/rank ?rank]]
+            (d/q db (:card.rank/name rank))))
+      (ffirst)
+      (->> (d/entity db))))
