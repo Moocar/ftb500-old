5f11e0971815872bb59e04f997d17ee10cc1ed45
Anthony Marcar <amarcar@walmartlabs.com>
2014-11-13 21:45:23 -0800

    card-name card-suit

--- pure-game/src/me/moocar/ftb500/card.clj
+++ pure-game/src/me/moocar/ftb500/card.clj
@@ -1,10 +1,16 @@
 (ns me.moocar.ftb500.card
   (:require [me.moocar.ftb500.schema :refer [card?]]))
 
+(defn- rank-name [card]
+  (:card.rank/name (:card/rank card)))
+
+(defn- suit-name [card]
+  (:card.suit/name (:card/suit card)))
+
 (defn card=
   [card1 card2]
   {:pre [(card? card1) (card? card2)]}
-  (and (= (:card.rank/name (:card/rank card1))
-          (:card.rank/name (:card/rank card2)))
-       (= (:card.suit/name (:card/suit card1))
-          (:card.suit/name (:card/suit card2)))))
+  (and (= (rank-name card1)
+          (rank-name card2))
+       (= (suit-name card1)
+          (suit-name card2))))
