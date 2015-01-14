dcf3ca578fba71b377958659df8832e7684915dc
Anthony Marcar <amarcar@walmartlabs.com>
2015-01-13 17:51:18 -0800

    moved ai logic to client

--- ai/src/me/moocar/ftb500/client/ai.clj
+++ ai/src/me/moocar/ftb500/client/ai.clj
@@ -3,13 +3,10 @@
             [clojure.set :refer [rename-keys]]
             [me.moocar.async :refer [<? go-try]]
             [me.moocar.ftb500.bid :as bid]
-            [me.moocar.ftb500.client :refer [game-send! send!]]
+            [me.moocar.ftb500.client :as client :refer [send!]]
             [me.moocar.ftb500.client.ai.bids :as bids]
             [me.moocar.ftb500.client.ai.tricks :as tricks]
             [me.moocar.ftb500.client.ai.schema :refer [ai?]]
-            [me.moocar.ftb500.seats :as seats]
-            [me.moocar.ftb500.schema :as schema
-             :refer [game? seat? bid? player? uuid? ext-card? card?]]
             [me.moocar.lang :refer [uuid]]
             [me.moocar.log :as log]))
 
@@ -43,97 +40,16 @@
    (log ai "Stopped ai client")
    ai))
 
-(defn touch-game
-  [game]
-  (update-in game [:game/deck :deck/cards] #(map schema/touch-card %)))
-
-(defn game-info
-  [this game-id]
-  (go-try (touch-game (second (<? (send! this :game-info {:game-id game-id}))))))
-
-(defn join-game
-  "Attempts to join the game in [:game :game/id]. If player is already
-  joined, the seat they are assigned to is immediately returned.
-  Otherwise, an available seat is selected and an attempt to join the
-  game is made. If successful, the seat is returned, otherwise a new
-  seat is selected and tried again, in a loop"
-  [{:keys [player game] :as ai}]
-  {:pre [(ai? ai)]}
-  (let [game-id (:game/id game)]
-    (go-try
-      (loop [game game]
-        (or (seats/find-assigned game player)
-            (if-let [seat (seats/find-available game)]
-              (do
-                (<! (game-send! (assoc ai :seat seat)
-                                :join-game {:game/id game-id}))
-                (recur (<? (game-info ai game-id))))
-              (throw (ex-info "No more seats available"))))))))
-
-(defn get-deal-cards
-  "Takes the deal-cards message and retrieves the first-seat and dealt
-  hand and associates them back into the ai map"
-  [ai deal-cards]
-  {:pre [(ai? ai)]}
-  (let [{:keys [game]} ai
-        {:keys [body]} deal-cards
-        {:keys [seat/cards]} body
-        _ (assert (every? ext-card? cards))
-        first-seat (seats/find (:game/first-seat body) game)
-        hand (set (map schema/touch-card cards))]
-    (-> ai
-        (assoc-in [:seat :seat/cards] hand)
-        (assoc-in [:game :game/first-seat] first-seat))))
-
-(defn ready-game
-  "Initiates the ai map with the basic game information"
-  [ai game-id]
-  {:pre [(:transport ai)
-         (uuid? game-id)]}
-  (let [{:keys [route-pub-ch]} ai]
-    (go-try
-     (-> ai
-         (assoc :game (<? (game-info ai game-id)))
-         (as-> ai
-               (assoc ai :game/num-players (count (:game/seats (:game ai)))))))))
-
-(defn wait-on-joins
-  "Waits for all players to join the game and returns all the seats"
-  [join-game-ch ai]
-  {:pre [(ai? ai)]}
-  (go-try 
-   (->> join-game-ch
-        (async/take (:game/num-players ai))
-        (async/into [])
-        (<?)
-        (map :body)
-        (sort-by :seat/position))))
-
-(defn join-game-and-wait-for-others
-  "Joins the game in ai and waits for all players to join the game,
-  and for the hand to be dealt"
-  [ai]
-  {:pre [(ai? ai)]}
-  (let [{:keys [route-pub-ch]} ai
-        join-game-ch (async/chan)
-        deal-cards-ch (async/chan)]
-    (async/sub route-pub-ch :join-game join-game-ch)
-    (async/sub route-pub-ch :deal-cards deal-cards-ch)
-    (go-try
-     (as-> ai ai
-           (assoc ai :seat (<? (join-game ai)))
-           (assoc-in ai [:game :game/seats] (<? (wait-on-joins join-game-ch ai)))
-           (get-deal-cards ai (<? deal-cards-ch))))))
-
 (defn start-playing
   "Join a game, wait for all players to join, run the bidding round,
-  and then the tricks roundq"
+  and then the tricks round"
   [ai game-id]
   {:pre [(:transport ai)]}
   (go-try
-   (-> (ready-game ai game-id)
+   (-> (client/ready-game ai game-id)
        <?
-       (join-game-and-wait-for-others)
+       (as-> ai
+           (client/join-game-and-wait-for-others ai (client/join-game ai)))
        <?
        (bids/start)
        <?
--- client/src/me/moocar/ftb500/client.clj
+++ client/src/me/moocar/ftb500/client.clj
@@ -1,5 +1,10 @@
 (ns me.moocar.ftb500.client
-  (:require [me.moocar.async :as moo-async :refer [<? go-try]]
+  (:require [clojure.core.async :as async :refer [<!]]
+            [me.moocar.async :as moo-async :refer [<? go-try]]
+            [me.moocar.ftb500.client.schema :refer [client?]]
+            [me.moocar.ftb500.schema :as schema :refer [seat? ext-card?]]
+            [me.moocar.ftb500.seats :as seats]
+            [me.moocar.lang :refer [uuid?]]
             [me.moocar.log :as log]))
 
 (defn log [client msg]
@@ -28,3 +33,113 @@
                   :seat/id (:seat/id (:seat client)))]
     (log client {:route route :msg new-msg})
     (send! client route new-msg)))
+
+(defn touch-game
+  [game]
+  (update-in game
+             [:game/deck :deck/cards]
+             #(map schema/touch-card %)))
+
+(defn game-info
+  [this game-id]
+  (go-try
+   (-> (send! this :game-info {:game-id game-id})
+       <?
+       second
+       touch-game)))
+
+(defn join-game
+  "Attempts to join the game in [:game :game/id]. If player ha already
+  joined, the seat they are assigned to is immediately returned.
+  Otherwise, an available seat is selected and an attempt to join the
+  game is made. If successful, the seat is returned, otherwise a new
+  seat is selected and tried again, in a loop.
+
+  If a seat is specified, an attempt will be made to join that seat.
+  If someone else has taken it, nil is returned"
+
+  ;; seat specified
+  ([{:keys [player game route-pub-ch] :as client}
+    seat]
+   {:pre [(client? client) (seat? seat)]}
+   (let [game-id (:game/id game)]
+     (go-try
+      (if-let [assigned (seats/find-assigned game player)]
+        (if-not (seats/player= player (:seat/player assigned))
+          (throw (ex-info "you've already been assigned to seat"
+                          {:seat assigned}))
+          assigned)
+        (do
+          (<? (game-send! (assoc client :seat seat)
+                          :join-game {:game/id game-id}))
+          (assoc seat :user/id (:user/id player)))))))
+
+  ;; seat not specified
+  ([{:keys [player game] :as client}]
+   {:pre [(client? client)]}
+   (let [game-id (:game/id game)]
+     (go-try
+      (loop [game game]
+        (if-let [seat (seats/find-available game)]
+          (let [seat (<! (join-game client seat))]
+            (if-let [error (ex-data seat)]
+              (if (= [:seat-taken] (:error error))
+                (recur (<? (game-info client game-id)))
+                (throw seat))
+              seat))
+          (throw (ex-info "No more seats available"))))))))
+
+(defn get-deal-cards
+  "Takes the deal-cards message and retrieves the first-seat and dealt
+  hand and associates them back into the client"
+  [client deal-cards]
+  {:pre [(client? client)]}
+  (let [{:keys [game]} client
+        {:keys [body]} deal-cards
+        {:keys [seat/cards]} body
+        _ (assert (every? ext-card? cards))
+        first-seat (seats/find (:game/first-seat body) game)
+        hand (set (map schema/touch-card cards))]
+    (-> client
+        (assoc-in [:seat :seat/cards] hand)
+        (assoc-in [:game :game/first-seat] first-seat))))
+
+(defn ready-game
+  "Initiates the ai map with the basic game information"
+  [client game-id]
+  {:pre [(:transport client)
+         (uuid? game-id)]}
+  (let [{:keys [route-pub-ch]} client]
+    (go-try
+     (-> client
+         (assoc :game (<? (game-info client game-id)))
+         (as-> client
+               (assoc client :game/num-players (count (:game/seats (:game client)))))))))
+
+(defn wait-on-joins
+  "Waits for all players to join the game and returns all the seats"
+  [join-game-ch client]
+  {:pre [(client? client)]}
+  (go-try
+   (->> join-game-ch
+        (async/take (:game/num-players client))
+        (async/into [])
+        (<?)
+        (map :body)
+        (sort-by :seat/position))))
+
+(defn join-game-and-wait-for-others
+  "Joins the game in ai and waits for all players to join the game,
+  and for the hand to be dealt"
+  [client joined-seat-ch]
+  {:pre [(client? client)]}
+  (let [{:keys [route-pub-ch]} client
+        join-game-ch (async/chan)
+        deal-cards-ch (async/chan)]
+    (async/sub route-pub-ch :join-game join-game-ch)
+    (async/sub route-pub-ch :deal-cards deal-cards-ch)
+    (go-try
+     (as-> client client
+       (assoc client :seat (<? joined-seat-ch))
+       (assoc-in client [:game :game/seats] (<? (wait-on-joins join-game-ch client)))
+       (get-deal-cards client (<? deal-cards-ch))))))
